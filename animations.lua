9787196dc37da9b72c4bd75c566c98efc69f7328
fliskScript <100136481+fliskScript@users.noreply.github.com>
2025-08-01 15:45:11 +0300

    Create animations.lua

---
+++ animations.lua
@@ -0,0 +1,6 @@
+local fakeanim = Instance.new("Animation")
+fakeanim.AnimationId = "rbxassetid://939385007"
+local noAnim = Instance.new("Animation")
+noAnim.AnimationId = "rbxassetid://0"
+
+return fakeanim, noAnim
